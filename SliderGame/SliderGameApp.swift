de800710533e180e22ce6133edb36ab6e3d1ab56
IgorPrivalikhin <iprivalihin@mail.ru>
2023-01-22 17:17:28 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Анастасия Булдакова on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
